ab5b39467019111846794a2d8836980acf93e8ac
etwicaksono <ekoteguhwicaksono@gmail.com>
2024-03-22 10:09:59 +0700

    Setup main function

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,23 @@ package main
 
 import (
 	"context"
+	"os"
+	"os/signal"
 	"store-hexagon-architecture/config"
 	"store-hexagon-architecture/internal/connection"
+	productMongo "store-hexagon-architecture/internal/domain/products/mongo"
+	"store-hexagon-architecture/internal/handler/api"
+	productHnd "store-hexagon-architecture/internal/handler/api/products"
 	"store-hexagon-architecture/internal/infrastructure"
+	"store-hexagon-architecture/internal/pkg/http"
+	productSvc "store-hexagon-architecture/internal/service/products"
 	"store-hexagon-architecture/internal/utils/logger"
+	"store-hexagon-architecture/internal/utils/validatorutil"
+	"syscall"
+	"time"
+
+	"github.com/gofiber/contrib/otelfiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
@@ -20,13 +33,10 @@ func main() {
 		Config: cfg,
 		Logger: lgr,
 	}
-	_, err := conn.NewMongoDB()
+	mongoDB, err := conn.NewMongoDB()
 	if err != nil {
 		return
 	}
-
-	// Init repository
-	// Init service
 
 	ctx := context.Background()
 
@@ -43,4 +53,47 @@ func main() {
 	defer func() {
 		err = otelShutdown(ctx)
 	}()
+
+	// Initialize web server
+	httpServer := http.New(http.Config{
+		Host:         cfg.GetString("app.host"),
+		Port:         cfg.GetString("app.port"),
+		IdleTimeout:  time.Duration(cfg.GetInt("fiber.idleTimeout")) * time.Second,
+		WriteTimeout: time.Duration(cfg.GetInt("fiber.writeTimeout")) * time.Second,
+		ReadTimeout:  time.Duration(cfg.GetInt("fiber.readTimeout")) * time.Second,
+		Prefork:      cfg.GetBool("fiber.prefork"),
+	})
+	router := httpServer.Router()
+	router.Use(recover.New(recover.Config{
+		EnableStackTrace: cfg.GetBool("fiber.enableStackTrace"),
+	}))
+	router.Use(otelfiber.Middleware())
+
+	// Initialize  validator
+	vld := validatorutil.GetValidator()
+
+	// Initialize repository
+	productRepo := productMongo.RepoProvider(mongoDB, "products", &otl, lgr)
+
+	// Initialize service
+	productService := productSvc.ServiceProvider(&otl, vld, productRepo)
+
+	// Initialize handler
+	productHandler := productHnd.HandlerProvider(lgr, otl, productService)
+
+	// Register api route.
+	api.SetRoutes(router, productHandler)
+
+	// Run web server.
+	httpServerChan := httpServer.Run()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	select {
+	case err := <-httpServerChan:
+		if err != nil {
+			return
+		}
+	case <-sigChan:
+	}
 }
